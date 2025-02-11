44f9fa0315f4576f4f355abad50b81d713edfd45
Benjamin Lovering <benjaminlovering1@gmail.com>
2025-02-11 13:00:46 +0000

    API framework started

---
+++ bin/backend.dart
@@ -0,0 +1,18 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart';
+
+void main() async {
+  var handler = const Pipeline().addMiddleware(logRequests()).addHandler(handler);
+  var service = Service();
+  var server = await shelf_io.serve(service.handler, 'localhost', 8080);
+}
+
+class Service {
+  Handler get handler {
+    final route = Router();
+    
+
+    return route;
+  }
+}
